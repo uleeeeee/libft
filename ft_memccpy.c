8a7d50149c925736637e0642f451b6105cabc745
uleeeeee <ule@hotmail.fi>
2021-11-11 21:04:12 +0200

    ft_memccpy.c

---
+++ ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tulenius <tulenius@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 19:49:08 by tulenius          #+#    #+#             */
+/*   Updated: 2021/11/11 21:04:02 by tulenius         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+	size_t	i;
+	char	*tmp_dest;
+	char	*tmp_src;
+
+	if (!dest || !src)
+		return (NULL);
+	i = 0;
+	tmp_dest = (char *)dest;
+	tmp_src = (char *)src;
+	while (i < n)
+	{
+		tmp_dest[i] = tmp_src[i];
+		if ((unsigned char)tmp_src[i] == (unsigned char)c)
+			return ((char *)dest + i + 1);
+		i++;
+	}
+	return (NULL);
+}
