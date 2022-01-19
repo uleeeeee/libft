f04125d91b72b2c27bbc110d9293f01af4b92218
Toni Ulenius <59621642+uleeeeee@users.noreply.github.com>
2022-01-19 12:21:53 +0200

    Add files via upload
    missing bonus functions

---
+++ libft/ft_lstdel.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tulenius <tulenius@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/21 12:51:24 by tulenius          #+#    #+#             */
+/*   Updated: 2021/12/21 13:10:18 by tulenius         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*tmp;
+	t_list	*next;
+
+	tmp = *alst;
+	while (tmp != NULL)
+	{
+		next = tmp->next;
+		del(tmp->content, tmp->content_size);
+		free(tmp);
+		tmp = next;
+	}
+	*alst = NULL;
+}
