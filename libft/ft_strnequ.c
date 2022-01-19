98b4dec1fdb10edc45477fa318b29661d63a935a
Toni Ulenius <toni@Tonis-MacBook-Air.local>
2022-01-19 13:13:09 +0200

    norm fix

--- libft/ft_strnequ.c
+++ libft/ft_strnequ.c
@@ -6,13 +6,13 @@
 /*   By: tulenius <tulenius@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 14:52:13 by tulenius          #+#    #+#             */
-/*   Updated: 2022/01/19 12:48:16 by tulenius         ###   ########.fr       */
+/*   Updated: 2022/01/19 13:12:58 by tulenius         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strnequ(char const *s1, char const *s2, size_t n)
+int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	if (!s1 || !s2)
 		return (0);
